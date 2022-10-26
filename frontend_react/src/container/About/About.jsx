4ecf1681c53fb17b79cf3f1affb438671c7d45e0
M1chalS <michalszajner@wp.pl>
2022-10-26 22:46:45 +0200

    Integracja z Sanity

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -3,15 +3,17 @@ import { motion } from 'framer-motion';
 
 import './About.scss';
 import {images} from "../../constants";
-
-const abouts = [
-    { title: 'Web development', description: 'I am a good web developer.', imgUrl: images.about01},
-    { title: 'Web Design', description: 'I am a good web developer.', imgUrl: images.about02},
-    { title: 'UI/UX', description: 'I am a good web developer.', imgUrl: images.about03},
-    { title: 'Web Animations', description: 'I am a good web developer.', imgUrl: images.about04}
-]
+import { urlFor, client} from '../../client';
 
 const About = () => {
+    const [abouts, setAbouts] = useState([]);
+    useEffect(() => {
+        const query = '*[_type == "abouts"]';
+
+        client.fetch(query)
+            .then(data => setAbouts(data))
+    }, []);
+
     return (
         <>
             <h2 className="head-text">
@@ -27,7 +29,7 @@ const About = () => {
                         className="app__profile-item"
                         key={about.title + index}
                     >
-                        <img src={about.imgUrl} alt={about.title} />
+                        <img src={ urlFor(about.imgUrl)} alt={about.title} />
                         <h2 className="bold-text" style={{ marginTop: 20}}>{about.title}</h2>
                         <p className="p-text" style={{ marginTop: 10}}>{about.description}</p>
                     </motion.div>
